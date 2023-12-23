35be7623141f75aadf163fc98139ef6c45041dbb
YadokBlack <kodaygames@gmail.com>
2023-12-23 13:56:59 +0100

    Crear escenario Sokoban.

--- Assets/Scripts/Sokoban.cs
+++ Assets/Scripts/Sokoban.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class Sokoban : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] prefabs;
+    public GameObject padre;
+
+    int alturaImagen = 60;
+    const int tamanyo = 10;
+    int[,] datosMapa;
+
+    private void Start()
     {
-        
+        AsignarValoresIniciales();
+
+        MostrarMapa();
     }
 
-    // Update is called once per frame
-    void Update()
+    void AsignarValoresIniciales()
     {
-        
+        datosMapa = new int[tamanyo, tamanyo]
+        {
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            {2, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+            {2, 1, 0, 0, 0, 0, 0, 0, 1, 2},
+            {2, 1, 0, 4, 0, 5, 3, 0, 1, 2},
+            {2, 1, 0, 0, 0, 0, 0, 0, 1, 2},
+            {2, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+        };
+    }
+
+    void MostrarMapa()
+    {
+        for (int x = 0; x < tamanyo; x++)
+        {
+            for (int y = 0; y < tamanyo; y++)
+            {
+                int valor = datosMapa[x, y];
+                if (valor >= 0 && valor < prefabs.Length)
+                {
+
+                    if (valor > 3)
+                    {
+                        Instantiate(prefabs[0], new Vector3((x + 0.5f )* alturaImagen, (y + 0.5f) * alturaImagen, 0), Quaternion.identity, padre.transform);
+                    }
+                    Instantiate(prefabs[valor], new Vector3((x + 0.5f) * alturaImagen, (y+0.5f)*alturaImagen,0), Quaternion.identity, padre.transform);                    
+                }
+            }
+        }
     }
 }
