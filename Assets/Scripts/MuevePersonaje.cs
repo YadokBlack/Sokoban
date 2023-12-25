344bc70702ec4cf633702c38c4aaa302b87520d8
YadokBlack <kodaygames@gmail.com>
2023-12-25 13:57:29 +0100

    Crear MuevePersonaje.cs y actualizar Sokoban.cs para permitir movimiento personaje y cajas

---
+++ Assets/Scripts/MuevePersonaje.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuevePersonaje : MonoBehaviour
+{
+    public float velocidad = 1.0f;
+    public Sokoban sokobanScript;
+    bool puedeMover;
+
+    private void Start()
+    {
+        puedeMover = false;
+        sokobanScript = FindObjectOfType<Sokoban>();
+
+        if (sokobanScript == null)
+        {
+            Debug.LogError("No se encontró el script Sokoban en la escena.");
+        }
+    }
+
+    private void Update()
+    {
+        MoverPersonaje();
+    }
+
+    public bool Mover(Vector2 direccion)
+    {
+        if (Mathf.Abs(direccion.x) < 0.5f)
+        {
+            direccion.x = 0;
+        }
+        else
+        {
+            direccion.y = 0;
+        }
+        direccion.Normalize();
+        if(Bloqueado(transform.position, direccion))
+        {
+            return false;
+        }
+        else
+        {
+            if (ValorCasilla(transform.position, direccion) == 4)
+            {
+                Debug.Log("Caja para mover!.");
+                MoverCaja(transform.position, direccion);
+            }
+            Vector2 movimiento = direccion * sokobanScript.alturaImagen;
+            transform.Translate(movimiento);
+
+            return true;
+        }
+    }
+
+    int ValorCasilla(Vector3 position, Vector2 direccion)
+    {
+        int x = Mathf.FloorToInt(position.x / sokobanScript.alturaImagen);
+        int y = Mathf.FloorToInt(position.y / sokobanScript.alturaImagen);
+
+        int siguienteX = x + Mathf.RoundToInt(direccion.x);
+        int siguienteY = y + Mathf.RoundToInt(direccion.y);
+
+        int valor = sokobanScript.datosMapa[siguienteX, siguienteY];
+        return valor;
+    }
+
+    bool Bloqueado(Vector3 position, Vector2 direccion)
+    {
+        int x = Mathf.FloorToInt(position.x / sokobanScript.alturaImagen);
+        int y = Mathf.FloorToInt(position.y / sokobanScript.alturaImagen);
+
+        Debug.Log("Coordenadas: " + x + ", " + y);
+
+        int siguienteX = x + Mathf.RoundToInt(direccion.x);
+        int siguienteY = y + Mathf.RoundToInt(direccion.y);
+
+        Debug.Log("Siguiente Coordenadas: " + siguienteX + ", " + siguienteY);
+
+        if (siguienteX < 0 || siguienteX >= Sokoban.tamanyo || siguienteY < 0 || siguienteY >= Sokoban.tamanyo)
+        {
+            return false;
+        }
+
+        int valor = sokobanScript.datosMapa[siguienteX, siguienteY];
+        Debug.Log("En la posicion " + siguienteX + "," + siguienteY + " = " + valor);
+
+        // Si la celda siguiente contiene una caja (valor 4)
+        if (valor == 4)
+        {
+            int siguiente2X = siguienteX + Mathf.RoundToInt(direccion.x);
+            int siguiente2Y = siguienteY + Mathf.RoundToInt(direccion.y);
+
+            if (siguiente2X < 0 || siguiente2X >= Sokoban.tamanyo || siguiente2Y < 0 || siguiente2Y >= Sokoban.tamanyo)
+            {
+                return false;
+            }
+
+            int siguiente2Valor = sokobanScript.datosMapa[siguiente2X, siguiente2Y];
+            Debug.Log("Valor2 = " + siguiente2Valor);
+
+            return siguiente2Valor == 2 || siguiente2Valor == 1;
+        }
+
+        // Si la celda siguiente no es una caja, verificar si es una pared (valor 1) o bloqueo (valor 2)
+        return valor == 2 || valor == 1;
+    }
+
+    void MoverCaja(Vector3 position, Vector2 direccion)
+    {
+        int siguienteX = Mathf.FloorToInt(position.x / sokobanScript.alturaImagen) + Mathf.RoundToInt(direccion.x);
+        int siguienteY = Mathf.FloorToInt(position.y / sokobanScript.alturaImagen) + Mathf.RoundToInt(direccion.y);
+
+        Debug.Log("Caja en:" + siguienteX + "," + siguienteY);
+
+        float realX = (siguienteX + 0.5f) * sokobanScript.alturaImagen;
+        float realY = (siguienteY + 0.5f) * sokobanScript.alturaImagen;
+        float diferencia = sokobanScript.alturaImagen * 0.25f;
+
+        GameObject caja = sokobanScript.cajas.Find(c => Vector2.Distance(new Vector2(c.transform.position.x, c.transform.position.y), new Vector2(realX, realY)) < diferencia);
+        
+        Debug.Log("cuantas cajas:" + sokobanScript.cajas.Count);
+        Debug.Log("Posicion Real:" + realX + "," + realY);
+
+        if (caja != null )
+        {
+            Debug.Log("caja encontrada!");
+
+            int siguiente2X = siguienteX + Mathf.RoundToInt(direccion.x);
+            int siguiente2Y = siguienteY + Mathf.RoundToInt(direccion.y);
+
+            sokobanScript.datosMapa[siguienteX, siguienteY] = 0; // La caja deja su posición actual
+            sokobanScript.datosMapa[siguiente2X, siguiente2Y] = 4; // La caja ocupa la nueva posición
+
+            float real2X = (siguiente2X + 0.5f) * sokobanScript.alturaImagen;
+            float real2Y = (siguiente2Y + 0.5f) * sokobanScript.alturaImagen;
+
+            Debug.Log("Posicion anterior: " + caja.transform.position);
+
+            caja.transform.position = new Vector3(real2X, real2Y, caja.transform.position.z);
+            Debug.Log("Posicion nueva: " + caja.transform.position);
+        }
+        else
+        {
+            Debug.Log("No encontrada!!");
+        }
+
+    }
+
+    void MoverPersonaje()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        move.Normalize();
+
+        if (move.sqrMagnitude > 0.5f )
+        {
+            if (puedeMover)
+            {
+                puedeMover = false;
+                Mover(move);
+            }
+        }
+        else
+        {
+            puedeMover = true;
+        }
+    }
+
+    bool EsPosicionValida(int x, int y)
+    {
+        if (x < 0 || x >= Sokoban.tamanyo || y < 0 || y >= Sokoban.tamanyo)
+        {
+            return false; 
+        }
+
+        int valor = sokobanScript.datosMapa[x, y];
+
+        // 1 = pared  2 = arbol
+        return valor != 2 && valor != 1; 
+    }
+}
+
--- Assets/Scripts/Sokoban.cs
+++ Assets/Scripts/Sokoban.cs
@@ -8,9 +8,11 @@ public class Sokoban : MonoBehaviour
     public GameObject[] prefabs;
     public GameObject padre;
 
-    int alturaImagen = 60;
-    const int tamanyo = 10;
-    int[,] datosMapa;
+    public int alturaImagen = 60;
+    public const int tamanyo = 10;
+    public int[,] datosMapa;
+
+    public List<GameObject> cajas;
 
     private void Start()
     {
@@ -44,13 +46,38 @@ public class Sokoban : MonoBehaviour
             {
                 int valor = datosMapa[x, y];
                 if (valor >= 0 && valor < prefabs.Length)
-                {
-
+                {                    
                     if (valor > 3)
                     {
                         Instantiate(prefabs[0], new Vector3((x + 0.5f )* alturaImagen, (y + 0.5f) * alturaImagen, 0), Quaternion.identity, padre.transform);
                     }
-                    Instantiate(prefabs[valor], new Vector3((x + 0.5f) * alturaImagen, (y+0.5f)*alturaImagen,0), Quaternion.identity, padre.transform);                    
+                    else
+                    {
+                        Instantiate(prefabs[valor], new Vector3((x + 0.5f) * alturaImagen, (y + 0.5f) * alturaImagen, 0), Quaternion.identity, padre.transform);
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < tamanyo; x++)
+        {
+            for (int y = 0; y < tamanyo; y++)
+            {
+                int valor = datosMapa[x, y];
+                if (valor >= 0 && valor < prefabs.Length)
+                {                    
+                    if (valor > 3)
+                    {
+                        GameObject objeto;
+                        objeto = Instantiate(prefabs[valor], new Vector3((x + 0.5f) * alturaImagen, (y + 0.5f) * alturaImagen, 0), Quaternion.identity, padre.transform);
+
+                        if (valor == 4)
+                        {
+                            Debug.Log("Caja en:" + x + "," + y);
+                            Debug.Log("Caja position:" + objeto.transform.position);
+                            cajas.Add(objeto);
+                        }
+                    }
                 }
             }
         }
