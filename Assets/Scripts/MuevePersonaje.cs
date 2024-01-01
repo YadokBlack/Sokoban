94647699c76fb52a5deafeb948471d3825b125a0
YadokBlack <kodaygames@gmail.com>
2024-01-01 13:44:28 +0100

    Extraer ColisionMapa de MuevePersonaje.cs

--- Assets/Scripts/MuevePersonaje.cs
+++ Assets/Scripts/MuevePersonaje.cs
@@ -2,11 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class MovimientoCaja : MonoBehaviour 
+public class ColisionMapa : MonoBehaviour
 {
-    // caja
-    void MoverCaja(Vector3 position, Vector2 direccion)
+    public Sokoban sokobanScript;
+    const int sinColision = 0;
+    const int colisionMovil = 1;
+    const int conColision = 2;
+
+    public ColisionMapa(Sokoban soko)
+    {
+        sokobanScript = soko;
+    }
+
+    public void MueveCajaSiColisiona(Vector3 position, Vector2 direccion)
+    {
+        if (ColisionaConCaja(direccion))
+        {
+            MoverCaja(transform.position, direccion);
+        }
+    }
+
+    private bool ColisionaConCaja(Vector2 direccion)
+    {
+        return ValorCasillaColision(transform.position, direccion) == colisionMovil;
+    }
+
+    private int ValorCasillaColision(Vector3 position, Vector2 direccion)
+    {
+        int siguienteX = SiguientePosicion(position.x, direccion.x);
+        int siguienteY = SiguientePosicion(position.y, direccion.y);
+        int valor = sokobanScript.datosMapaColision[siguienteX, siguienteY];
+        return valor;
+    }
+
+    public bool Bloqueado(Vector3 position, Vector2 direccion)
+    {
+        int siguienteX = SiguientePosicion(position.x, direccion.x);
+        int siguienteY = SiguientePosicion(position.y, direccion.y);
+
+        if (FueraDeRango(siguienteX, siguienteY)) return false;
+
+        int valor = sokobanScript.datosMapaColision[siguienteX, siguienteY];
+
+        return (ColisionaConCaja(valor)) ? ColisionDetrasCaja(siguienteX, siguienteY, direccion) : ColisionaConMuro(valor);
+    }
+
+    private static bool ColisionaConCaja(int valor)
+    {
+        return valor == colisionMovil;
+    }
+
+    private static bool ColisionaConMuro(int valor)
+    {
+        return valor == conColision;
+    }
+
+    private bool ColisionDetrasCaja(int siguienteX, int siguienteY, Vector2 direccion)
+    {
+        int siguiente2X = siguienteX + Mathf.RoundToInt(direccion.x);
+        int siguiente2Y = siguienteY + Mathf.RoundToInt(direccion.y);
+
+        if (FueraDeRango(siguiente2X, siguiente2Y)) return false;
+
+        int siguiente2Valor = sokobanScript.datosMapaColision[siguiente2X, siguiente2Y];
+        return ColisionaConMuro(siguiente2Valor) || ColisionaConCaja(siguiente2Valor);
+    }
+
+    private static bool FueraDeRango(int siguienteX, int siguienteY)
+    {
+        return siguienteX < 0 || siguienteX >= Sokoban.tamanyo || siguienteY < 0 || siguienteY >= Sokoban.tamanyo;
+    }
+    private int SiguientePosicion(float position, float direccion)
+    {
+        return Mathf.FloorToInt(position / sokobanScript.alturaImagen) + Mathf.RoundToInt(direccion);
+    }
+
+    public void MoverCaja(Vector3 position, Vector2 direccion)
     {
         int siguienteX = SiguientePosicion(position.x, direccion.x);
         int siguienteY = SiguientePosicion(position.y, direccion.y);
@@ -21,7 +92,6 @@ public class MovimientoCaja : MonoBehaviour
         DesplazarCaja(direccion, siguienteX, siguienteY, caja);
     }
 
-    // caja 
     private void DesplazarCaja(Vector2 direccion, int siguienteX, int siguienteY, GameObject caja)
     {
         int siguiente2X = siguienteX + Mathf.RoundToInt(direccion.x);
@@ -34,81 +104,12 @@ public class MovimientoCaja : MonoBehaviour
     }
 }
 
-public class MapaColision : MonoBehaviour
-{
-    public Sokoban sokobanScript;
-    const int sinColision = 0;
-    const int colisionMovil = 1;
-    const int conColision = 2;
-
-    // colision, mapa o caja
-    private bool ColisionaConCaja(Vector2 direccion)
-    {
-        return ValorCasillaColision(transform.position, direccion) == colisionMovil;
-    }
-
-    // colision o mapa
-    private int ValorCasillaColision(Vector3 position, Vector2 direccion)
-    {
-        int siguienteX = SiguientePosicion(position.x, direccion.x);
-        int siguienteY = SiguientePosicion(position.y, direccion.y);
-        int valor = sokobanScript.datosMapaColision[siguienteX, siguienteY];
-        return valor;
-    }
-
-    // colision o mapa
-    private bool Bloqueado(Vector3 position, Vector2 direccion)
-    {
-        int siguienteX = SiguientePosicion(position.x, direccion.x);
-        int siguienteY = SiguientePosicion(position.y, direccion.y);
-
-        if (FueraDeRango(siguienteX, siguienteY)) return false;
-
-        int valor = sokobanScript.datosMapaColision[siguienteX, siguienteY];
-
-        return (ColisionaConCaja(valor)) ? ColisionDetrasCaja(siguienteX, siguienteY, direccion) : ColisionaConMuro(valor);
-    }
-
-    // colision o mapa
-    private static bool ColisionaConCaja(int valor)
-    {
-        return valor == colisionMovil;
-    }
-
-    // colision o mapa
-    private static bool ColisionaConMuro(int valor)
-    {
-        return valor == conColision;
-    }
-
-    // colision, mapa o caja
-    private bool ColisionDetrasCaja(int siguienteX, int siguienteY, Vector2 direccion)
-    {
-        int siguiente2X = siguienteX + Mathf.RoundToInt(direccion.x);
-        int siguiente2Y = siguienteY + Mathf.RoundToInt(direccion.y);
-
-        if (FueraDeRango(siguiente2X, siguiente2Y)) return false;
-
-        int siguiente2Valor = sokobanScript.datosMapaColision[siguiente2X, siguiente2Y];
-        return ColisionaConMuro(siguiente2Valor) || ColisionaConCaja(siguiente2Valor);
-    }
-
-    // mapa
-    private static bool FueraDeRango(int siguienteX, int siguienteY)
-    {
-        return siguienteX < 0 || siguienteX >= Sokoban.tamanyo || siguienteY < 0 || siguienteY >= Sokoban.tamanyo;
-    }
-    private int SiguientePosicion(float position, float direccion)
-    {
-        return Mathf.FloorToInt(position / sokobanScript.alturaImagen) + Mathf.RoundToInt(direccion);
-    }
-}
-
 
 public class MuevePersonaje : MonoBehaviour
 {
     public Sokoban sokobanScript;
     public ContadorPasos contadorPasos;
+    public ColisionMapa colisionMapa;
     public float velocidad = 1.0f;    
     bool puedeMover;
      
@@ -131,6 +132,7 @@ public class MuevePersonaje : MonoBehaviour
         {
             Debug.LogError("No se encontró el Contador de Pasos.");
         }
+        colisionMapa = new ColisionMapa(sokobanScript);     
     }
 
     private void Update()
@@ -138,7 +140,6 @@ public class MuevePersonaje : MonoBehaviour
         MoverPersonaje();
     }
 
-    // personaje
     public void Mover(Vector2 direccion)
     {
         if (Mathf.Abs(direccion.x) < 0.5f)
@@ -150,19 +151,16 @@ public class MuevePersonaje : MonoBehaviour
             direccion.y = 0;
         }
         direccion.Normalize();
-        if(!Bloqueado(transform.position, direccion))
+        if(!colisionMapa.Bloqueado(transform.position, direccion))
         {
             DesplazaPersonaje(direccion);            
         }
     }
 
-    // personaje
     private void DesplazaPersonaje(Vector2 direccion)
     {
-        if (ColisionaConCaja(direccion))
-        {
-            MoverCaja(transform.position, direccion);
-        }
+        colisionMapa.MueveCajaSiColisiona(transform.position, direccion);
+
         Vector2 movimiento = direccion * sokobanScript.alturaImagen;
         transform.Translate(movimiento);
 
@@ -170,7 +168,6 @@ public class MuevePersonaje : MonoBehaviour
         contadorPasos.Aumenta();
     }
 
-    // personaje
     void MoverPersonaje()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
